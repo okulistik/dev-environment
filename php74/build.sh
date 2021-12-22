7b3b8e8f4824b9ae6d3f8d5fbcfcca5812b23371
Doğan Can <dgncan@gmail.com>
2021-12-22 15:51:05 +0300

    imp: upadate build.sh

--- php74/build.sh
+++ php74/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 mkdir -p ~/work/conf
+mkdir -p ~/work/conf/bup
+mkdir -p ~/work/public
 touch ~/work/conf/blank-alias.conf
+touch ~/work/conf/bup/blank-alias.conf
+
 docker build -t okulistik/php74 .
